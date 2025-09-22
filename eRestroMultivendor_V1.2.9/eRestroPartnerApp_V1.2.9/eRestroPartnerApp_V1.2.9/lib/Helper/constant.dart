ff6b0deb3302861eca28c1612277a02045bae23c
Ogaufi4 <ogaufimokopakgosi4@gmail.com>
2025-09-22 10:25:01 +0200

    EDIT:same as customer app

---
+++ eRestroMultivendor_V1.2.9/eRestroPartnerApp_V1.2.9/eRestroPartnerApp_V1.2.9/lib/Helper/constant.dart
@@ -0,0 +1,17 @@
+const String appName = 'eKonnect Partner App';
+
+const String packageName = 'com.konnect.erestropartner';
+
+const String iosPackage = 'com.konnect.erestropartner';
+
+// demo
+const String baseUrl = 'https://flowbasetech.co.bw/konnect/partner/app/v1/api/';
+
+const int timeOut = 50;
+
+String? countryCode = '+267';
+
+const int perPage = 10;
+
+const String BANK_DETAIL =
+    'Bank Details:\nAccount No :123XXXXX\nIFSC Code: 123XXX \nName: Abc xyz';
